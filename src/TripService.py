ac0b43f612e403e27bc7c3cb962b6c99109f2bf7
Mateo Lamthi <mateo.lamthi@outlook.com>
2024-10-14 16:38:28 +0200

    Refactored TripService.py to make the code better

--- src/TripService.py
+++ src/TripService.py
@@ -1,21 +1,21 @@
-from TripDAO import TripDAO
-from UserSession import UserSession
 from UserNotLoggedInException import UserNotLoggedInException
+from UserSession import UserSession
+from TripDAO import TripDAO
 
 class TripService:
-  def getTripsByUser(self, user):
-    logged_user = UserSession.getInstance().getLoggedUser()
-    isFriend = False
-    tripList = []
-    if logged_user:
-      for friend in user.getFriends():
-        if friend is logged_user:
-          isFriend = True
-          break;
-      if isFriend:
-        tripList = TripDAO.findTripsByUser(user)
-      return tripList
-    else:
-      raise UserNotLoggedInException()
 
+    def get_trips_by_user(self, user):
+        logged_in_user = UserSession.get_instance().get_logged_user()
+        self._validate_logged_in_user(logged_in_user)
 
+        if self._is_friend(logged_in_user, user):
+            return TripDAO.find_trips_by_user(user)
+        else:
+            return []
+
+    def _validate_logged_in_user(self, user):
+        if user is None:
+            raise UserNotLoggedInException()
+
+    def _is_friend(self, logged_in_user, user):
+        return user in logged_in_user.get_friends()
