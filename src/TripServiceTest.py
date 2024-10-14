01da9eaf0dda1664ab7c1dc6d9fb26a238866988
Mateo Lamthi <mateo.lamthi@outlook.com>
2024-10-14 16:40:07 +0200

    Write unit tests for TripService with mocks for UserSession and TripDAO, deleted old not so good tests

--- src/TripServiceTest.py
+++ src/TripServiceTest.py
@@ -1,25 +1,44 @@
 import unittest
+from unittest.mock import patch
 
-from src.TripService import TripService
-from src.User import User
-from src.UserNotLoggedInException import UserNotLoggedInException
+from Trip import Trip
+from TripService import TripService
+from User import User
+from UserNotLoggedInException import UserNotLoggedInException
 
 
-class TestTripService(unittest.TestCase):
+class TripServiceTest(unittest.TestCase):
 
     def setUp(self):
         self.trip_service = TripService()
-        self.logged_in_user = User()
-        self.target_user = User()
+        self.user = User()
 
-    def test_user_not_logged_in_raises_exception(self):
+    @patch('UserSession.get_instance')
+    def test_should_throw_exception_when_user_is_not_logged_in(self, mock_session):
+        mock_session.return_value.get_logged_user.return_value = None
         with self.assertRaises(UserNotLoggedInException):
-            self.trip_service.get_trips_by_user(None, self.target_user)
+            self.trip_service.get_trips_by_user(self.user)
 
-    def test_no_friends_returns_empty_trip_list(self):
-        result = self.trip_service.get_trips_by_user(self.logged_in_user, self.target_user)
+    @patch('UserSession.get_instance')
+    @patch('TripDAO.find_trips_by_user')
+    def test_should_return_trips_when_users_are_friends(self, mock_find_trips, mock_session):
+        friend = User()
+        trip = Trip()
+
+        mock_session.return_value.get_logged_user.return_value = self.user
+        self.user.add_friend(friend)
+        mock_find_trips.return_value = [trip]
+
+        result = self.trip_service.get_trips_by_user(friend)
+        self.assertEqual(result, [trip])
+
+    @patch('UserSession.get_instance')
+    def test_should_return_empty_list_when_users_are_not_friends(self, mock_session):
+        mock_session.return_value.get_logged_user.return_value = self.user
+        not_friend = User()
+
+        result = self.trip_service.get_trips_by_user(not_friend)
         self.assertEqual(result, [])
 
-    def test_friends_with_no_trips_returns_empty_trip_list(self):
-        self.logged_in_user.addFriend(self.target_user)
-        result = self.trip
+if __name__ == '__main__':
+    unittest.main()
