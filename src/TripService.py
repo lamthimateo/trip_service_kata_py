18d7060a0df03aeb91562d38cc6666dca1f2bdb6
Mateo Lamthi <mateo.lamthi@outlook.com>
2024-10-14 16:56:55 +0200

    Implement new requirement to show trips only if users have traveled together

--- src/TripService.py
+++ src/TripService.py
@@ -1,6 +1,7 @@
-from UserNotLoggedInException import UserNotLoggedInException
-from UserSession import UserSession
 from TripDAO import TripDAO
+from UserSession import UserSession
+from UserNotLoggedInException import UserNotLoggedInException
+
 
 class TripService:
 
@@ -8,7 +9,7 @@ class TripService:
         logged_in_user = UserSession.get_instance().get_logged_user()
         self._validate_logged_in_user(logged_in_user)
 
-        if self._is_friend(logged_in_user, user):
+        if self._is_friend(logged_in_user, user) and self._have_traveled_together(logged_in_user, user):
             return TripDAO.find_trips_by_user(user)
         else:
             return []
@@ -19,3 +20,7 @@ class TripService:
 
     def _is_friend(self, logged_in_user, user):
         return user in logged_in_user.get_friends()
+
+    def _have_traveled_together(self, logged_in_user, user):
+        user_trips = TripDAO.find_trips_by_user(logged_in_user)
+        return any(trip in user_trips for trip in TripDAO.find_trips_by_user(user))
